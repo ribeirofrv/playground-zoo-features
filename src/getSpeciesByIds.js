d5b5f3eb992d2f4f86e8cf481c9b701da1da624f
Fran Ribeiro <ribeirofrv@gmail.com>
2022-03-16 23:08:31 -0300

    Requisito 1 Em Desenvolvimento

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+/** Funcão(RECEBE ID) retorna dados dos animais.
+ * Percorre o array de entradas
+ * Compara o array de entradas com os dados de species
+ * Verifica se existe algum valor dentro de species identico ao valor de entrada
+ * Retorna valores identicos
+ *
+ * Erro: Função retorna um array vazio além do valor esperado.
+ */
+
+const getSpeciesByIds = (...entries) => {
+  const { ...animals } = data.species; // busca dados de especies.
+  // console.log(entries);
+  return entries.filter((ids) => ids === animals);
+};
 
 module.exports = getSpeciesByIds;
