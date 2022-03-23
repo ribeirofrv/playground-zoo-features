a93a982c9dedaad017db34b63cd677d3741c3cd5
Fran Ribeiro <ribeirofrv@gmail.com>
2022-03-23 19:35:02 -0300

    Cumpre Requisito 1 :sunglasses: ~amem

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,18 +1,9 @@
 const data = require('../data/zoo_data');
 
-/** Funcão(RECEBE ID) retorna dados dos animais.
- * Percorre o array de entradas
- * Compara o array de entradas com os dados de species
- * Verifica se existe algum valor dentro de species identico ao valor de entrada
- * Retorna valores identicos
- *
- * Erro: Função retorna um array vazio além do valor esperado.
- */
-
-const getSpeciesByIds = (...entries) => {
-  const { ...animals } = data.species; // busca dados de especies.
-  // console.log(entries);
-  return entries.filter((ids) => ids === animals);
-};
+/* Mapeia cada item do array de entradas
+* Encontra no objeto data.species a chave identica ao codigo passado por parametro
+*/
+const getSpeciesByIds = (...ids) => ids.map((cod) => data.species.find((key) => key.id === cod));
+// *lint odeia linhas gigantes por isso diminui o nome das variaveis
 
 module.exports = getSpeciesByIds;
