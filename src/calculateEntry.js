e90ef4f13e680add5f0675e1316f5ee4563f5cd4
Fran Ribeiro <ribeirofrv@gmail.com>
2022-03-22 22:50:48 -0300

    Cumpre Requisito 6 :satisfied:

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aquii
-}
+const countEntrants = (entrants) => {
+  const countPerson = { child: 0, adult: 0, senior: 0 };
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+  entrants.forEach((person) => {
+    if (person.age < 18) countPerson.child += 1;
+    if (person.age >= 18 && person.age < 50) countPerson.adult += 1;
+    if (person.age >= 50) countPerson.senior += 1;
+  });
+
+  return countPerson;
+};
+
+const calculateEntry = (entrants) => {
+  const { ...price } = data.prices;
+
+  // Se 'entrants' for verdade E contenha alguma chave o calculo é realizado.
+  if (entrants && Object.keys(entrants).length > 0) {
+    const { child, adult, senior } = countEntrants(entrants);
+    return (child * price.child) + (adult * price.adult) + (senior * price.senior);
+  }
+
+  return 0;
+};
 
 module.exports = { calculateEntry, countEntrants };
