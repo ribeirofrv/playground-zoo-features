3cb6a6c816eb46cb6727115888573708714b1a02
Fran Ribeiro <ribeirofrv@gmail.com>
2022-03-23 15:50:32 -0300

    Cumpre Requisito 2 :smile:

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const getAnimalsOlderThan = (name, age) => {
+  const [...species] = data.species;
+  const animal = species.find((fera) => name === fera.name); // Encontra e guarda dados da especie especifica
 
+  return animal.residents.every((resident) => age <= resident.age); // Verifica se TODOS residentes daquela especie tem idade maior ou igual à passada pelo parametro
+};
+
+// console.log(getAnimalsOlderThan('frogs', 2));
+// console.log(getAnimalsOlderThan('frogs', 3));
 module.exports = getAnimalsOlderThan;
