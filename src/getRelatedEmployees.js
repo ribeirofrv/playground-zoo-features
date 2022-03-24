cf73be13e56cfbfbcb31ab45be6a23e7835f466f
Fran Ribeiro <ribeirofrv@gmail.com>
2022-03-23 23:40:38 -0300

    Cumpre Requisito 4 :upside_down_face: ~amem

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+// Verifica se o Id corresponde à uma pessoa gerente retornando um valor bool
+const isManager = (id) => data.employees.some((value) => value.managers.includes(id));
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+const getRelatedEmployees = (managerId) => {
+  // Se a função isManager for verdade retorna uma lista de funcionários da pessoa gerente
+  if (isManager(managerId)) {
+    return data.employees
+      .filter((person) => person.managers.includes(managerId))
+      .map((ppl) => `${ppl.firstName} ${ppl.lastName}`);
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!'); // linha dada pelo requisito.
+};
 
 module.exports = { isManager, getRelatedEmployees };
