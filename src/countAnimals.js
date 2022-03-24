1d18d688eaa2c946ca6d81c99baf537b46078faa
Fran Ribeiro <ribeirofrv@gmail.com>
2022-03-24 18:09:22 -0300

    Cumpre Requisito 5 :upside_down_face: ~a ordem dos fatores as vezes alteram o resultado...

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,29 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+// Função gera objeto com nomes e quantidade de animais de cada espécie
+const getAllAnimals = () => {
+  const animalsData = species.reduce((acc, curr) => {
+    acc[curr.name] = curr.residents.length;
+    return acc;
+  }, {});
+
+  return animalsData;
+};
+
+// Função retorna número de população de animais do zoo
+const countAnimals = (animal) => {
+  // Se o parâmetro for falso retorna função que traz um objeto com nomes e quantidade de animais de cada espécie
+  if (!animal) return getAllAnimals();
+
+  // Busca dados do animal recebido por parâmetro
+  const getAnimal = species.find((key) => key.name === animal.specie);
+
+  // Se existir uma chave 'sex' no objeto recebido por parâmetro, retorna quantidade de animais do sexo específico
+  if (animal.sex) {
+    return getAnimal.residents.filter((key) => key.sex === animal.sex).length;
+  }
+
+  return getAnimal.residents.length;
+};
 
 module.exports = countAnimals;
