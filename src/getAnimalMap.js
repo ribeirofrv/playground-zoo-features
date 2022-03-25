3e48593fc7c726e483e61db4dfbec26714dfd12a
Fran Ribeiro <ribeirofrv@gmail.com>
2022-03-24 21:33:34 -0300

    Requisito 7 - [loading 3/7...]

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,33 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const categorizedAnimals = () => {
+  const animalsMap = species.reduce((acc, curr) => {
+    // Se não existir uma key no objeto correspondente ao indice atual, ela é criada
+    if (!acc[curr.location]) {
+      acc[curr.location] = [];
+    }
 
+    acc[curr.location].push(curr.name); // Adiciona animal ao array da localização correspondente
+
+    return acc;
+  }, {});
+  // console.log(animalsMap);
+  return animalsMap;
+};
+
+const getAnimalMap = (options) => {
+  if (!options || !options.includeNames) return categorizedAnimals();
+
+  if (options.sex === 'female' && options.sorted) return categorizedAnimals();
+
+  return 0;
+};
+
+// console.log(categorizedAnimals());
+// {
+//   NE: [ 'lions', 'giraffes' ],
+//   NW: [ 'tigers', 'bears', 'elephants' ],
+//   SE: [ 'penguins', 'otters' ],
+//   SW: [ 'frogs', 'snakes' ]
+// }
 module.exports = getAnimalMap;
