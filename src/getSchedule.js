8df6a597d3f29af4f6c61a3d53221467465711dc
Fran Ribeiro <ribeirofrv@gmail.com>
2022-04-02 10:18:12 -0300

    Cumpre requisito 8 - Implementa função `getSchedule` :star_struck:

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -9,30 +9,46 @@ const animalDays = species.reduce((acc, curr) => {
 /** Dias da Semana */
 const daysOfTheWeek = Object.keys(hours);
 
-/** Agenda do Zoo */
+/** Agenda do Zoo para ser acessada por dia especifico */
 const schedule = daysOfTheWeek.reduce((acc, curr) => {
   const day = curr;
   acc[curr] = {
-    [daysOfTheWeek[curr]]: {
+    [day]: {
       officeHour: `Open from ${hours[curr].open}am until ${hours[curr].close}pm`,
       exhibition: species.filter((animal) => (animal.availability.includes(day)))
         .map(({ name }) => name),
     },
   };
+  if (day === 'Monday') {
+    acc[day] = {
+      [day]: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+    };
+  }
 
   return acc;
 }, {});
 
-schedule.Monday.officeHour = 'CLOSED';
-schedule.Monday.exhibition = 'The zoo will be closed!';
+/** Agenda Completa do Zoo */
+const allSchedule = daysOfTheWeek.reduce((acc, curr) => {
+  const day = curr;
+  acc[curr] = {
+    officeHour: `Open from ${hours[curr].open}am until ${hours[curr].close}pm`,
+    exhibition: species.filter((animal) => (animal.availability.includes(day)))
+      .map(({ name }) => name),
+  };
+  if (day === 'Monday') acc[day] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
 
+  return acc;
+}, {});
+
+/** Lista de Animais do Zoo */
 const animal = Object.keys(animalDays);
 
 const getSchedule = (target) => {
   if (daysOfTheWeek.includes(target)) return schedule[target];
   if (animal.includes(target)) return animalDays[target];
-  return schedule;
+  return allSchedule;
 };
-console.log(getSchedule('Tuesday'));
+// console.log(getSchedule('looooooooool'));
 
 module.exports = getSchedule;
